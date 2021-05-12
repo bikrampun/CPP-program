61d3271b3911682c582828c605abf8e2fd43b14d
bikrampun <bikrampun47@gmail.com>
2021-05-12 22:20:00 +0545

    Inheriting from fixDeposit class and main program written

--- inheritAccount.cpp
+++ inheritAccount.cpp
@@ -70,3 +70,69 @@ public:
 		cout<<"\nBalance after Current deposit: "<<blc;
 	}
 };
+class ShortTerm: public FixedDeposit
+{
+public:
+	ShortTerm(){}
+	ShortTerm(char *n,int acc,float balance,float in1): FixedDeposit(n,acc,balance)
+	{
+		interest=in1;
+	}
+	void display(){
+		Account::display();
+		blc+=interest;
+		cout<<"\nBalance after ShortTerm FixedDeposit: "<<blc;
+	}
+};
+class LongTerm: public FixedDeposit
+{
+public:
+	LongTerm(){}
+	LongTerm(char *n,int acc,float balance,float in2): FixedDeposit(n,acc,balance)
+	{
+		interest=in2;
+	}
+	void display(){
+		Account::display();
+		blc+=interest;
+		cout<<"\nBalance after LongTerm FixedDeposit: "<<blc;
+	}
+};
+int main()
+{
+	int choice,ch;
+	Savings s("Bikram",1234,2000,20.10);
+	ShortTerm st("Bikram",1234,2000,100);
+	LongTerm lt("Bikram",1234,2000,200);
+	Current c("Bikram",1234,2000,0);
+	cout<<"\nEnter choice for creating account \n1. Savings deposit \n2. FixedDeposit \n3. Current deposit\n\n";
+	cin>>choice;
+	switch(choice)
+	{
+		case 1:
+			s.display();
+			break;
+		case 2:
+			cout<<"\nEnter choice for FixedDeposit \n1. ShortTerm \n2. LongTerm\n\n";
+			cin>>ch;
+			switch(ch)
+			{
+				case 1:
+					st.display();
+					break;
+				case 2:
+					lt.display();
+					break;
+				default:
+					cout<<"\nPlease enter correct choice.";
+			}
+			break;
+		case 3:
+			c.display();
+			break;
+		default:
+			cout<<"\nPlease enter correct choice.";
+	}
+	getch();
+	return 0;
+}
