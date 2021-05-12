4069b5b4374d5d687f901e1d25c29557e9d6753d
bikrampun <bikrampun47@gmail.com>
2021-05-12 15:05:08 +0545

    Class is created to find area

---
+++ inheritOverride.cpp
@@ -0,0 +1,28 @@
+/*3. Define a class Rectangle withdata members to represent
+length and breadth and member function to input and
+calculate the area. Derive class Box with appropriate
+data member to represent height and member function to
+calculate surface area. Use same function name input() and
+calculate() in both the classes and implement function overriding.*/
+#include <iostream>
+#include <conio.h>
+using namespace std;
+class Rectangle
+{
+protected:
+	float length,breadth,area;
+public:
+	void input(){
+		cout<<"\nEnter length:";
+		cin>>length;
+		cout<<"Enter breadth:";
+		cin>>breadth;
+	}
+	void calculate(){
+		area=length*breadth;
+		cout<<"\nArea="<<area<<endl;
+	}
+	void display(){
+		cout<<"\nThe length: "<<length<<" and breadth: "<<breadth<<endl;
+	}
+};
